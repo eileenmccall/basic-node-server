6d8c344a925023617ca22d8b148d6028e404bdbc
joshmccall <josh@wewritecode.com>
2017-12-24 22:31:14 -0600

    no longer in maintenance mode

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.use((req, res, next) => {
 	next()
 });
 
-app.use((req, res, next) => {
-	res.render('maintenance.hbs', {
-		pageTitle: 'Maintenance'
-	});
-});
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs', {
+// 		pageTitle: 'Maintenance'
+// 	});
+// });
 
 app.use(express.static(__dirname + '/public'));
 
