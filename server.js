099225b01931364cf46d9148b46b260bb874b88c
joshmccall <josh@wewritecode.com>
2017-12-24 22:19:21 -0600

    creating git repository before deployment

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('unable to append file');
+		}
+	});
+	next()
+});
+
+app.use((req, res, next) => {
+	res.render('maintenance.hbs', {
+		pageTitle: 'Maintenance'
+	});
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('upperCase', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home',
+		welcomeMessage: 'Welcome to hell'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		error: 'Bad request'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Now listening on port 3000');
+});
